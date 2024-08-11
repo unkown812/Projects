59cdab4804dc7d972bf741ff0cbc3e419effb4ef
unkown812 <102848825+unkown812@users.noreply.github.com>
2024-08-11 20:15:46 +0530

    Create Shoppingsystem.cpp

---
+++ Shoppingsystem.cpp
@@ -0,0 +1,13 @@
+#include<iosstream>
+#include<fstream>
+#include<iomanip>
+#include<dos.h>
+#include<graphic.h>
+#include<string>
+
+class Product{
+    int ProductId;
+    int HSNnumber;
+    int Price;
+    string ProductName;
+}
